dc612047626a5f1c3b168367d4bcc655f4ce51fd
William Ho <72125448+WillyBilly06@users.noreply.github.com>
2024-08-27 22:44:37 +0700

    Update ESP32_Equalizer_SigmaDSP.ino

--- 3_Second_order_EQ/ESP32_Equalizer_SigmaDSP.ino
+++ 3_Second_order_EQ/ESP32_Equalizer_SigmaDSP.ino
@@ -199,6 +199,10 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+
+   DO NOT REUSE THIS CODE WITHOUT "TRADEMARK" AND AUTHOR NAME.  
+
+   
    */
 
 
